97af14b47c925d43ebf104c341245bc485e62915
= <santiagomarini44@gmail.com>
2021-07-10 01:36:14 -0300

    replaced else sentence by return

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -15,45 +15,46 @@ class AuthController {
 
     if (!user || !passwordMatch) {
       res.status(400).send({error: 'Invalid user or password.'});
-    } else {
-      const token = jwt.sign({id: user._id}, config.JWT.SECRET_KEY, {
-        expiresIn: (60 * 60) //expires in an hour
-      });
-      res.send({token: token});
+      return;
     }
+    const token = jwt.sign({id: user._id}, config.JWT.SECRET_KEY, {
+      expiresIn: (60 * 60) //expires in an hour
+    });
+
+    res.send({token: token});
   }
 
   async signUp(req, res) {
     const {name, email, password} = req.body;
     if (!name || !email || !password) {
-      res.status(400).send({error: 'Name, email and password are required.'})
-    } else {
-
-      // check if user already exists
-      const userExists = await User.findOne({
-        email: email
-      });
-
-      if (userExists) {
-        res.status(400).send({error: `User with email '${email}' already exists.`});
-      } else {
-
-        // if not exist, create a new user
-        const newUser = new User({
-          name: name,
-          email: email,
-          password: await User.encryptPassword(password)
-        });
-
-        // save created user to db
-        const insertedUser = await newUser.save();
-
-        // return userId
-        res.send({
-          userId: insertedUser._id
-        })
-      }
+      res.status(400).send({error: 'Name, email and password are required.'});
+      return;
     }
+
+    // check if user already exists
+    const userExists = await User.findOne({
+      email: email
+    });
+
+    if (userExists) {
+      res.status(400).send({error: `User with email '${email}' already exists.`});
+      return;
+    }
+
+    // if not exist, create a new user
+    const newUser = new User({
+      name: name,
+      email: email,
+      password: await User.encryptPassword(password)
+    });
+
+    // save created user to db
+    const insertedUser = await newUser.save();
+
+    // return userId
+    res.send({
+      userId: insertedUser._id
+    })
   }
 }
 
